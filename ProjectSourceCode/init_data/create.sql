46b4165b9cc1e417910eb594533f37b4f6dfe0b6
Oliver Schmitz <olsc5113@colorado.edu>
2025-11-05 12:11:41 -0700

    updated sql tables for scoreboard and users

--- ProjectSourceCode/init_data/create.sql
+++ ProjectSourceCode/init_data/create.sql
@@ -1,6 +1,15 @@
 CREATE TABLE IF NOT EXISTS users (
     username VARCHAR(100) PRIMARY KEY,
     password VARCHAR(100) NOT NULL,
-    name VARCHAR(100),
+    first_name VARCHAR(100),
+    last_name VARCHAR(100),
     email VARCHAR(100)
 );
+
+CREATE TABLE IF NOT EXITST scoreboard (
+    first_name VARCHAR(100),
+    last_name VARCHAR(100),
+    score INT,
+    FOREIGN KEY(username) REFERENCES users(username)
+);
+
