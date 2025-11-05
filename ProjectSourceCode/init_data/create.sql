2780360abbe14464187edb56fca1d485ffc79e9c
TaydenB <tabr1565@colorado.edu>
2025-11-05 15:58:45 -0700

    Added linking table between scoreboard and users

--- ProjectSourceCode/init_data/create.sql
+++ ProjectSourceCode/init_data/create.sql
@@ -8,12 +8,17 @@ CREATE TABLE IF NOT EXISTS users (
 );
 
 CREATE TABLE IF NOT EXITST scoreboard (
-    user_id INT PRIMARY KEY,
+    scoreboard_id INT PRIMARY KEY,
     first_name VARCHAR(100),
     last_name VARCHAR(100),
     score INT,
     FOREIGN KEY(username) REFERENCES users(username)
 );
+
+CREATE TABLE IF NOT EXIST users_to_scoreboard (
+    user_id INT,
+    scoreboard_id INT;
+)
 
 
 
