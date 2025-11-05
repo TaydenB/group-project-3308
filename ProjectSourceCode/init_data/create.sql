e9fe39d045e67f991047c8661750d665234aeafa
TaydenB <tabr1565@colorado.edu>
2025-11-05 16:04:12 -0700

    Added friends table

--- ProjectSourceCode/init_data/create.sql
+++ ProjectSourceCode/init_data/create.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS users (
-    user_id INT PRIMARY KEY,
+    id INT PRIMARY KEY,
     username VARCHAR(100),
     password VARCHAR(100) NOT NULL,
     first_name VARCHAR(100),
@@ -7,17 +7,23 @@ CREATE TABLE IF NOT EXISTS users (
     email VARCHAR(100)
 );
 
-CREATE TABLE IF NOT EXITST scoreboard (
-    scoreboard_id INT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS scoreboard (
+    id INT PRIMARY KEY,
     first_name VARCHAR(100),
     last_name VARCHAR(100),
     score INT,
     FOREIGN KEY(username) REFERENCES users(username)
 );
 
-CREATE TABLE IF NOT EXIST users_to_scoreboard (
+CREATE TABLE IF NOT EXISTS users_to_scoreboard (
     user_id INT,
     scoreboard_id INT;
+)
+
+CREATE TABLE IF NOT EXISTS friends(
+    user_id INT,
+    friend_id INT,
+    status VARCHAR(20) DEFAULT 'pending',
 )
 
 
