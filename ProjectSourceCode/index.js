cc28b5b57d18db74210ef4c65c07baae71e2599e
Joe Olapurath <ojoev23@gmail.com>
2025-11-06 11:43:22 -0700

    Added files for testing

--- ProjectSourceCode/index.js
+++ ProjectSourceCode/index.js
@@ -84,5 +84,6 @@ app.use('/', require('./routes/profile'));
 // <!-- Section 5 : Start Server-->
 // *****************************************************
 // starting the server and keeping the connection open to listen for more requests
-app.listen(3000);
 console.log('Server is listening on port 3000');
+
+module.exports = app.listen(3000);
