4bf8cfe1dda57f2485366863cea217a8eaa5ad19
TaydenB <tabr1565@colorado.edu>
2025-11-17 15:48:55 -0700

    Added the keyboard key setup

--- ProjectSourceCode/views/resources/game.js
+++ ProjectSourceCode/views/resources/game.js
@@ -4,4 +4,9 @@ let selected_row = 0;
 let tile = 0;
 
 /*Create the implementation for the keyboard*/
-
+document.querySelectorAll(".keyboard-key").forEach(key => {
+    key.addEventListener("click", () => {
+        const letter = key.textContent.trim();
+        addLetter(letter);
+    })
+})
