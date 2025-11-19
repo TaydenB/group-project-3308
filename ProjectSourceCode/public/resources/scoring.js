3e7b83724a282b147ec1cb8b10393e4bd36d6c09
Oliver Schmitz <olsc5113@colorado.edu>
2025-11-19 15:26:21 -0700

    timer is updating every second calculating min and sec and showing result if needed might take out

--- ProjectSourceCode/public/resources/scoring.js
+++ ProjectSourceCode/public/resources/scoring.js
@@ -5,25 +5,36 @@ let guessesUsed = 0;
 let gameOver = false;
 let score = 0;
 
-/* TIMER STATE */
+/* timer state */
 let startTime = Date.now();
-let timerInterval = setInterval(updateTimer, 1000);
+let timerInterval = setInterval(updateTimer, 1000); //update every second
 
 function gettime(){
-    return Date.now() 
+    return Date.now() //gets local time
 }
 
 function timer(){
     if(gameOver){
-        clearInterval(TimeInterval);
+        clearInterval(TimeInterval); //game over finish game quit
         return;
     }
 
     const elapsed = getElapsedMs();
-    let remaining = TIME_LIMIT_MS - elapsed;
+    let remaining = TIME_LIMIT_MS - elapsed; //remaining time in the game
 
-    if(remaining <= 0){
-
+    if(remaining <= 0){ //if remaining time is <= 0 game is over
+        remaining = 0;
+        score = 0;
+        gameOver = true;
+        clearInterval(timerInterval);
     }
 
+    const totalSeconds = Math.floor(remaining / 1000); //remaining time in seconds
+    const minutes = Math.floor(totalSeconds / 60); //remaining time in min
+    const seconds = totalSeconds % 60; //remaining time in seconds but now can be used with min
+
+    const timerElem = document.getElementById("timer");
+    if (timerElem) {
+        timerElem.textContent = `${minutes}:${seconds.toString().padStart(2, "0")}`; //text for timer
+    }
 }
